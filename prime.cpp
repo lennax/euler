2aa99842c71cfe42e8e8a8bf85461440415a0401
James Goppert <jgoppert@l1.hsl.dynalias.com>
2011-07-30 19:23:42 -0400

    Realized I am stupid. VERY VERY STUPID

--- prime.cpp
+++ prime.cpp
@@ -9,9 +9,7 @@ public:
 	// constructor, using raii (resource acquisition is initialization)
 	PrimeFinder(uint32_t n) : 
 		_n(n), 
-		_primeTable(new uint32_t[n/32+1]) {
-			std::cout << "table 0: " << _primeTable[0] << std::endl;
-			std::cout << "table 1: " << _primeTable[1] << std::endl;
+		_primeTable(new uint32_t[n/_blockSize+1]) {
 			testStorage(1);
 			testStorage(2);
 		}
@@ -26,11 +24,20 @@ public:
 	// methods
 	void setPrime(uint32_t i, bool isPrime) {
 		std::cout << "block before: " << std::bitset<32>(_primeTable[i/_blockSize]) << std::endl;
-		if (~isPrime) { // 0 is prime, so set to 1
-			_primeTable[i/_blockSize] |= 1 << ((i-1) % _blockSize);	
-		} else { // 0 is prime, set to 0
-			_primeTable[i/_blockSize] &= ~(1 << ((i-1) % _blockSize));
+		if (isPrime) { 
+			std::cout << "is prime" << std::endl;
+			std::cout << "blockSize: " << _blockSize << std::endl;
+			std::cout << "i % blockSize: " << (i % _blockSize) << std::endl;
+			std::cout << "left shift: " << std::bitset<32>(1 << (i % _blockSize)) << std::endl;
+			std::cout << "rhs: " << std::bitset<32>(~(1 << (i % _blockSize))) << std::endl;
+			std::cout << "result: " << std::bitset<32>(_primeTable[i/_blockSize] & ~(1 << (i % _blockSize))) << std::endl;
+			_primeTable[i/_blockSize] &= ~(1 << (i % _blockSize));
+		} else { 
+			std::cout << "is not prime" << std::endl;
+			std::cout << "rhs: " << (1 << (i % _blockSize)) << std::endl;
+			_primeTable[i/_blockSize] |= 1 << (i % _blockSize);	
 		}
+		//_primeTable[i/_blockSize] = 3;
 		std::cout << "block after: " << std::bitset<32>(_primeTable[i/_blockSize]) << std::endl;
 	}
 
@@ -38,14 +45,14 @@ public:
 		// if test for bit is true, a non-zero power of 2 
 		// will be returned which will be cast to true
 		// FIXME: right half probably wrong
-		return bool(_primeTable[i/_blockSize] & (1 << ((i-1) % _blockSize)));	
+		return bool(_primeTable[i/_blockSize] & (1 << (i % _blockSize)));	
 	}
 
 	void testStorage(uint32_t i) {
 		std::cout << "Initial value of " << 
 			i <<  ": " << getPrime(1) << std::endl;
-		setPrime(1,true);
-		std::cout << "Set " << i << " to prime. New value: " << 
+		setPrime(1,false);
+		std::cout << "Set " << i << " to composite. New value: " << 
 			getPrime(1) << std::endl;
 	}
 
@@ -53,9 +60,9 @@ private:
 	// attributes
 	const uint32_t _n;
 	uint32_t * _primeTable; // alu typically atleast 32 bits
-	static const uint8_t _blockSize;
+	static const uint16_t _blockSize;
 };
-const uint8_t PrimeFinder::_blockSize = 32; // must match _primeTable type
+const uint16_t PrimeFinder::_blockSize = 8*sizeof(*_primeTable); // must match _primeTable type
 
 int main (int argc, char const* argv[])
 {
