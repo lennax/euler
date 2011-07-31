1d1d03fa9af49b713066673ed2cd57f42a7782eb
Lenna <lenna@l1.hsl.dynalias.com>
2011-07-31 19:48:59 -0400

    Working on triangle logic, incomplete.

--- p18.cpp
+++ p18.cpp
@@ -11,7 +11,7 @@ class PathFinder
 public:
 	PathFinder () {
 		using namespace std;
-		readFile2();
+		readFile();
 		cout << "triangle: " << endl;
 		for (int i=0;i<_triangle.size(); i++) {
 			for (int j=0;j<i+1;j++) cout << _triangle[i][j] << "\t";
@@ -20,24 +20,9 @@ public:
 	}
 private:
 	std::vector< std::vector<uint32_t> > _triangle;
-	void readFile() {
-		// import triangle and convert to 2D array
-		std::string line;
-		std::ifstream file; 
-		file.open ("triangle4.txt");
-		if (file.is_open()) {
-			std::cout << "File opened" << std::endl;
-			while (file.good()) {
-				std::cout << "Line good" << std::endl;
-				getline(file,line);
-				std::cout << line << std::endl;
-			}
-			file.close();
-		}
-		else std::cout << "Unable to open file" << std::endl;
-	}
 
-	void readFile2() {
+	// read file and convert to vector of vectors (2D array)
+	void readFile() {
 		using namespace std;
 		ifstream file;
 		file.open("triangle4.txt");
@@ -70,17 +55,21 @@ private:
 	}
 
 	// triangle addition logic
-	int getSum() {
-		int x;
-		int y;
-		if (x==0) {
-			// add x,y to x,y-1
-		}	
-		else if (x==y) {
-			// add x,y to x-1,y-1
-		}
-		else {
-			// add x,y to max((x,y-1), (x-1,y-1))
+	void getSum() {
+		for (int y=1;y<_triangle.size(); y++) for (int x=0;x<y+1;x++) 
+		{
+			if (x==0) {
+				_triangle[y][x] += _triangle[y-1][x];
+				// add x,y to x,y-1
+			}	
+			else if (x==y) {
+				_triangle[y][x] += _triangle[y-1][x-1];
+				// add x,y to x-1,y-1
+			}
+			else {
+				_triangle[y][x] += _triangle
+				// add x,y to max((x,y-1), (x-1,y-1))
+			}
 		}
 	}
 };
