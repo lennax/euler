ce05fe5d22be43a43f380b339e31a18b08bb1a67
Lenna Peterson <lennalenna@gmail.com>
2011-08-02 02:32:06 -0400

    p36 reverses strings. Fixed segfault.

--- src/p36.cpp
+++ src/p36.cpp
@@ -9,17 +9,24 @@
 //use bitset to treat as binary?
 
 bool isPalindrome(std::string number) {
-	
+	return false;
 }
 
 std::string strRev(std::string str) {
-	//int len = str.size();
-	std::cout << "length: " << str.size() << std::endl;
+	int len = str.size();
+	std::string reversed; 
+	for (int i=len-1; i>-1; i--) {
+		reversed.append(str.substr(i,1));
+	}
+	//std::cout << "rev: " << reversed << std::endl;
+	//std::cout << "length: " << str.size() << std::endl;
+	return reversed;
 }
 
 int main()
 {
 	std::string test = "1234567890";
-	strRev(test);
+	std::string testRev = strRev(test);
+	std::cout << test << " reversed: " << testRev << std::endl;
 	return 0;
 }
