d9b8f6882d74882946174103141e1d41ed5ba080
Lenna Peterson <lennalenna@gmail.com>
2011-08-01 22:49:38 -0400

    p19 gives 229, different from pl, still wrong.

--- src/p19.cpp
+++ src/p19.cpp
@@ -24,16 +24,26 @@ int main (int argc, char const* argv[])
 
 	//std::cout << months[1] << std::endl;
 	
+	uint32_t offset = 0;
+	uint32_t sundays = 0;
+
 	for (uint32_t year=1900; year<2001; year++) {
 		for (uint32_t month=0; month<12; month++) {
 			//need to implement isLeap()
+			if(month == 1 && isLeap(year)) {
+				offset += 29;
+			} else {
+				offset += months[month];
+			}
+			if (offset%7 == 6) sundays++;
 		}
 	}
-std::cout << "1000: " << isLeap(1000) << std::endl;
-std::cout << "2000: " << isLeap(2000) << std::endl;
-std::cout << "1004: " << isLeap(1004) << std::endl;
-std::cout << "1005: " << isLeap(1005) << std::endl;
+	//std::cout << "1000: " << isLeap(1000) << std::endl;
+	//std::cout << "2000: " << isLeap(2000) << std::endl;
+	//std::cout << "1004: " << isLeap(1004) << std::endl;
+	//std::cout << "1005: " << isLeap(1005) << std::endl;
 
+	std::cout << "Sundays on the first of the month: " << sundays << std::endl;
 
 	return 0;
 }
