0e2077f39826eca7cd5a2aadc43b4a0798d162ec
Lenna Peterson <lennalenna@gmail.com>
2011-08-02 02:18:54 -0400

    p36 has mysterious segfault.

--- src/p36.cpp
+++ src/p36.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+
 //585 is 1001001001 in binary - palindrome in both
 //find all numbers less than 1M palindromic in both
 //report sum
 //function isPalindrome() string reverse and compare??
 //use bitset to treat as binary?
+
+bool isPalindrome(std::string number) {
+	
+}
+
+std::string strRev(std::string str) {
+	//int len = str.size();
+	std::cout << "length: " << str.size() << std::endl;
+}
+
+int main()
+{
+	std::string test = "1234567890";
+	strRev(test);
+	return 0;
+}
