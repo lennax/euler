e15a0d0ef3b5d9d84e8dade8d83e78f57b49bf38
Lenna Peterson <lennalenna@gmail.com>
2011-08-02 13:30:27 -0400

    Cmd line, cast to binary works, leading zeroes?

--- src/p36.cpp
+++ src/p36.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <inttypes.h>
+#include <stdlib.h>
 
 //585 is 1001001001 in binary - palindrome in both
 //find all numbers less than 1M palindromic in both
@@ -23,10 +25,21 @@ std::string strRev(std::string str) {
 	return reversed;
 }
 
-int main()
+int strToInt(std::string str) {
+	int intReturn;
+	intReturn = atoi(str.c_str());
+	return intReturn;
+}
+
+int main (int argc, char const* argv[])
 {
-	std::string test = "1234567890";
-	std::string testRev = strRev(test);
-	std::cout << test << " reversed: " << testRev << std::endl;
+	using namespace std;
+	if (argc!=2) {
+		cerr << "usage: " << argv[0] << " number" << endl;
+		return 1;
+	}
+	string testRev = strRev(argv[1]);
+	cout << argv[1] << " reversed: " << testRev << endl;
+	cout << "bitset 32: " << bitset<32>(strToInt(argv[1])) << endl;
 	return 0;
 }
