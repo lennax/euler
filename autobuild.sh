eb38c53781c313ac0f26e6a90428d96b16d40bce
Lenna Peterson <lennalenna@gmail.com>
2011-08-05 22:25:01 -0400

    Modified autobuild.sh to accept arguments

--- autobuild.sh
+++ autobuild.sh
@@ -1,61 +1,78 @@
 #!/bin/bash
 
-PS3='Please enter your choice: '
-LIST="in_source_build install_build grab_debian_dependencies package_source package remake clean END"
 MAKEARGS="-j8"
-echo 
-echo 1\)in_source_build: is used for development.
-echo 2.\)install_build: is used for building before final installation to the system.
-echo 3.\)grab_debian_dependencies: installs all the required packages for debian based systems \(ubuntu maverick/ debian squeeze,lenny\)
-echo 4.\)package_source: creates a source package for distribution
-echo 5.\)package: creates binary packages for distribution
-echo 6.\)remake: calls make again after project has been configured as install or in source build
-echo 7.\)clean: removes the build directory 
 
-echo
-select OPT in $LIST
-do
-	if [ $OPT = "in_source_build" ] &> /dev/null
-	then
-		echo you chose in source build
-		mkdir -p build && cd build && cmake -DIN_SRC_BUILD:bool=TRUE .. && make $MAKEARGS
-		exit 0
-	elif [ $OPT = "install_build" ] &> /dev/null
-	then
-		echo you chose install build
-		mkdir -p build && cd build && cmake .. && make $MAKEARGS
-		exit 0
-	elif [ $OPT = "grab_debian_dependencies" ] &> /dev/null
-	then
-		echo you chose to install debian dependencies
-		sudo apt-get install cmake
-		exit 0
+function menu {
+	while [ 1 ] 
+	do
+		echo -n "Please enter your choice: " && read OPT
+		echo 
+		echo 1. in_source_build: is used for development.
+		echo 2. install_build: is used for building before final installation to the system.
+		echo 3. grab_debian_dependencies: installs all the required packages for debian based systems -- ubuntu maverick/ debian squeeze,lenny
+		echo 4. package_source: creates a source package for distribution
+		echo 5. package: creates binary packages for distribution
+		echo 6. remake: calls make again after project has been configured as install or in source build
+		echo 7. clean: removes the build directory 
+		echo 8. end
+	done
+}
 
-	elif [ $OPT = "remake" ] &> /dev/null
-	then
-		echo you chose to recall make on the previously configured build
-		cd build && make $MAKEARGS
-		exit 0
+function in_source_build {
+	echo you chose in source build
+	mkdir -p build && cd build && cmake -DIN_SRC_BUILD:bool=TRUE .. && make $MAKEARGS
+	exit 0
+}
+function install_build {
+	echo you chose install build
+	mkdir -p build && cd build && cmake .. && make $MAKEARGS
+	exit 0
+}
+function grab_debian_dependencies {
+	echo you chose to install debian dependencies
+	sudo apt-get install cmake
+	exit 0
+}
+function remake {
+	echo you chose to recall make on the previously configured build
+	cd build && make $MAKEARGS
+	exit 0	
+}
+function package_source {
+	echo you chose to package the source
+	install_build && make package_source
+	exit 0	
+}
+function package {
+	echo you chose to package the binary
+	install_build && make package
+	exit 0
+}
+function clean {
+	echo you chose to clean the build 
+	rm -rf build
+}
+function end_program {
+	exit 0
+}
 
-	elif [ $OPT = "package_source" ] &> /dev/null
-	then
-		echo you chose to package the source
-		mkdir -p build && cd build && cmake .. && make $MAKEARGS && make package_source
-		exit 0
 
-	elif [ $OPT = "package" ] &> /dev/null
-	then
-		echo you chose to package the binary
-		mkdir -p build && cd build && cmake .. && make $MAKEARGS && make package
-		exit 0
+if [ $# = 1 ]
+then 
+	OPT=$1
+else
+	menu
+fi
 
-	elif [ $OPT = "clean" ] &> /dev/null
-	then
-		echo you chose to clean the build 
-		rm -rf build
+case $OPT in 
+	1 ) in_source_build;;
+	2 ) install_build;;
+	3 ) grab_debian_dependencies;;
+	4 ) remake;;
+    5 ) package_source;;
+	6 ) package;;
+	7 ) clean;;
+	8 ) end_program;;	
+esac
 
-	elif [ $OPT = "END" ] &> /dev/null
-	then
-		exit 0
-	fi
-done
+
