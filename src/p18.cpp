b5a39b7fb7468fe5d4e9909ff524c064e92bf102
Lenna Peterson <lennalenna@gmail.com>
2011-08-06 03:41:36 -0400

    p18 works triangle/square min/max UGLY AS SIN.

--- src/p18.cpp
+++ src/p18.cpp
@@ -10,16 +10,22 @@
 class PathFinder 
 {
 public:
-	PathFinder (const char * fileName) {
+	enum pathType_t {
+		PATHTYPE_MIN, PATHTYPE_MAX
+	};	
+	PathFinder (const char * fileName, pathType_t pathType) {
+	// PathFinder (const char * fileName) {
 		using namespace std;
 		//cout << "filename: " << endl;
 		//std::string fileName;
 		//cin >> fileName;
 		readFile(string(fileName));
-
+		
 		cout << "Original matrix:" << endl;
 		printMatrix();	
-		uint32_t sum = getSum();
+
+		uint32_t sum = getSum(pathType);
+
 		cout << "Computed matrix:" << endl;
 		printMatrix();
 		cout << "sum: " << sum << endl;
@@ -31,7 +37,7 @@ private:
 		SHAPE_SQUARE, SHAPE_TRIANGLE, SHAPE_ERROR
 	};
 	
-	// read file and convert to vector of vectors (2D array)
+// read file and convert to vector of vectors (2D array)
 	void readFile(std::string fileName) {
 		using namespace std;
 		ifstream file;
@@ -72,7 +78,7 @@ private:
 		file.close();
 	}
 
-	// determine whether matrix is triangle or square
+// determine whether matrix is triangle or square
 	_shape_t getShape()
    	{
 		std::cout << "height: " << _matrix.size() << std::endl;
@@ -99,36 +105,50 @@ private:
 		}
 	}
 
-	// triangle addition logic
-	uint32_t getSum() {
+// triangle addition logic
+	uint32_t getSum(pathType_t pathType) {
 		_shape_t shape = getShape();
 		switch (shape) {
 			case SHAPE_TRIANGLE: 
 				for (int y=1;y<_matrix.size(); y++) for (int x=0;x<y+1;x++) 
 				{
+					uint32_t & cur = _matrix[y][x];
 					if (x==0) {
-						_matrix[y][x] += _matrix[y-1][x];
+						cur += _matrix[y-1][x];
 					} else if (x==y) {
-						_matrix[y][x] += _matrix[y-1][x-1];
+						cur += _matrix[y-1][x-1];
 					} else {
-						_matrix[y][x] += 
-							std::max(_matrix[y-1][x], _matrix[y-1][x-1]);
+						switch(pathType) {
+							case PATHTYPE_MIN:
+								cur += std::min(_matrix[y-1][x], _matrix[y-1][x-1]);
+								break;
+							case PATHTYPE_MAX:
+								cur += std::max(_matrix[y-1][x], _matrix[y-1][x-1]);
+								break;
+						};
 					}
 				}
 				break;
 			case SHAPE_SQUARE:
 				for (int y=0; y<_matrix.size(); y++) for (int x=0; x<_matrix.size(); x++)
 				{
+					uint32_t & cur = _matrix[y][x];
 					if (y == 0 && x == 0) {
 						continue;
 					}
 					else if (y == 0) {
-						_matrix[y][x] += _matrix[y][x-1];
+						cur += _matrix[y][x-1];
 					} else if (x == 0) {
-						_matrix[y][x] += _matrix[y-1][x];
+						cur += _matrix[y-1][x];
 					} else {
-						_matrix[y][x] += 
-							std::min(_matrix[y][x-1], _matrix[y-1][x]);
+						switch(pathType) {
+							case PATHTYPE_MIN: 
+								cur += std::min(_matrix[y][x-1], _matrix[y-1][x]);
+								break;
+							case PATHTYPE_MAX: 
+								cur += std::max(_matrix[y][x-1], _matrix[y-1][x]);
+								break;
+						}	
 					}
 				}
 				break;
@@ -138,10 +158,22 @@ private:
 				exit(1); 
 		}
 
-		uint32_t sum = 0;
-		for (int i = 0; i < _matrix.size(); i++) {
-			uint32_t current = _matrix[_matrix.size()-1][i];
-			if (current > sum) sum = current;
+		uint32_t sum;
+		if (shape == SHAPE_TRIANGLE) {
+			for (int i = 0; i < _matrix.size(); i++) {
+				uint32_t current = _matrix[_matrix.size()-1][i];
+				if (i == 0) sum = current;
+				switch(pathType) { 
+					case PATHTYPE_MIN: 
+						if (current < sum) sum = current;
+						break;
+					case PATHTYPE_MAX:
+						if (current > sum) sum = current;
+						break;
+				}
+			}
+		} else if (shape == SHAPE_SQUARE) {
+			sum = _matrix[_matrix.size()-1][_matrix.size()-1];
 		}
 		return sum;
 	}
@@ -151,11 +183,23 @@ private:
 int main (int argc, char const* argv[])
 {
 	using namespace std;
-	if (argc!=2) {
-		cerr << "usage: " << argv[0] << " filename" << endl;
+	if (argc!=3) {
+		cerr << "usage: " << argv[0] << " filename (min|max)" << endl;
 		return 1;
 	}
-	PathFinder test(argv[1]);
+	PathFinder::pathType_t pathType;
+	if (strcmp(argv[2], "min") == 0)
+			pathType = PathFinder::PATHTYPE_MIN;
+	else if (strcmp(argv[2], "max") == 0)
+			pathType = PathFinder::PATHTYPE_MAX;
+	else {
+			cerr << "usage: " << argv[0] << " filename (min|max)" << endl;
+			cerr << "Second argument must be 'min' or 'max'." << endl;
+			return 1;
+	}
+
+	PathFinder test(argv[1], pathType);
+	// PathFinder test(argv[1]);
 	return 0;
 }
 
