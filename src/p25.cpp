0fcea6b84b6b7c66275f0228a3619ceabeb2465a
Lenna Peterson <lennalenna@gmail.com>
2011-08-12 16:52:34 -0400

    p25 now properly recurses.
    Idiot-proofed range of seed: F(0) doesn't work, so continue;

--- src/p25.cpp
+++ src/p25.cpp
@@ -34,27 +34,30 @@ int main(int argc, char const* argv[]) {
 	mpf_class prev, cur, temp;
 	int rowNum = 0;
 	mp_exp_t maxExp;
-	for (uint32_t seed = 0; seed < 5000; seed++) {
+	for (uint32_t seed = 4500; seed < 5000; seed++) {
+		if ( seed == 0) {
+			continue; // F(0) and F(1) are both 0 
+		}
 		if (rowNum == 0) {
 			cur = fib(seed);
 			prev = fib(seed-1);
+			rowNum++;
 		} else {
 			temp = cur; 
 			cur += prev;
 			prev = temp;
 		}
+		mp_exp_t curExp = getExp(cur);
 		//if ( seed % 10 == 0 ) {
-			//mp_exp_t curExp = getExp(cur);
 			//std::cout << seed << "\t" << cur << "\t" << curExp << std::endl;
 		//}
-		mp_exp_t curExp = getExp(cur);
-		if (curExp > maxExp) {
-			maxExp = curExp;
-		}
 		if (curExp >= 1000) {
 			std::cout << "Term with >1000 digits: " << seed << std::endl;
-			break;
+			exit(0);
 		}
+		else if (curExp > maxExp) {
+			maxExp = curExp;
+		} 
 	}
 	std::cout << "max exp: " << maxExp << std::endl;
 
