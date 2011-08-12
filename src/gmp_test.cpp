b58804842ca9f61181955966d59cc5bee79f21c7
Lenna Peterson <lennalenna@gmail.com>
2011-08-12 15:58:23 -0400

    p25 works; overloaded pow() for mpf_class.

--- src/gmp_test.cpp
+++ src/gmp_test.cpp
@@ -1,16 +1,23 @@
 #include <iostream> // for cout, endl
 #include <math.h> // for pow()
-#include <gmp.h> // for mpf_t, mpf_set_ui()
+//#include <gmp.h> // for mpf_t, mpf_set_ui()
+#include <gmpxx.h> // for mpf_class
 #include <string> // for string
 
 int main() {
-	mpf_t fp;
-	mpf_init( fp );
+	//mpf_t fp;
+	//mpf_init( fp );
 	
 	//mpf_set_ui(fp, pow(10,1000));
-	mpf_set_d(fp, 10e10);
+	//mpf_set_d(fp, 10e10);
 
 	//mpf_out_str(stdout, 10, 2, fp);
+	
+	mpf_class fp, fp2;
+
+	fp = pow(5,12);
+	
+
 	std::cout << fp << std::endl;
 	return 0;
 }
--- src/p25.cpp
+++ src/p25.cpp
@@ -2,6 +2,7 @@
 // #include <algorithm>
 #include <math.h> // for sqrt(), floor(), pow()
 #include <inttypes.h> // for uint32_t
+#include <gmpxx.h> // for mpf_class etc
 #include <string> // for string
 #include "utilities.hpp" // for itoa()
 #include <stdlib.h> // for atoi()
@@ -12,27 +13,92 @@
 // = 1000 * log_2 (10) 
 // = 1000 * 3.3219
 
+mpf_class pow(mpf_class base, uint32_t exp) {
+	mpf_class result;
+	mpf_ptr resultPtr = result.get_mpf_t();
+	mpf_ptr basePtr = base.get_mpf_t();
+	mpf_pow_ui(resultPtr, basePtr, exp);
+	return result; 
+}
+
+// compute nth fibonacci term
+// only works for n<1480 (roughly)
+//mpf_class fib(int n) {
+	//mpf_class fib;
+	//double phi = (1+sqrt(5))/2;
+	//fib = floor( (pow(phi,n) - pow(1-phi,n) ) / sqrt(5) );
+	//return fib;
+//}
+mpf_class fib(int n) {
+	mpf_class fib;
+	mpf_class phi = (1+sqrt(5))/2;
+	fib = floor( (pow(phi,n) - pow(1-phi,n) ) / sqrt(5) );
+	return fib;
+}
+
+
+// return exponent
+mp_exp_t getExp(mpf_class f_class) {
+	mpf_ptr fl = f_class.get_mpf_t();
+	char * str;
+	mp_exp_t exp;
+	int base = 10;
+	size_t n_digits = 2;
+	mpf_get_str(str, &exp, base, n_digits, fl);
+	return exp;
+}
+
 int main(int argc, char const* argv[]) {
-	double phi = (1+sqrt(5))/2;
-	
-	// compute n and n-1, then recurse
-	int rowCount = 0;
-	long double prev = 0, cur = 0, temp = 0;
-	for (int seed = 1; seed < 21; seed++) {
-		if (rowCount == 0 ) {
-			cur = floor( (pow(phi,seed) - pow(1-phi,seed)) / sqrt(5) );
-			int p_seed = seed-1;
-			prev = floor( (pow(phi,p_seed) - pow(1-phi,p_seed)) / sqrt(5) );
-		} 
-		else { // if prev = 1; cur = 2
-			temp = cur; // temp = 2
-			cur += prev; // 2+1 = 3
-			prev = temp; // prev = 2
-			
+
+	mpf_class prev, cur, temp;
+	int rowNum = 0;
+	mp_exp_t maxExp;
+	for (int seed = 2000; seed < 20000; seed++) {
+		if (rowNum == 0) {
+			cur = fib(seed);
+			prev = fib(seed-1);
+		} else {
+			temp = cur; 
+			cur += prev;
+			prev = temp;
 		}
-		if ( seed % 1 == 0 ) 
-			std::cout << seed << "\t" << cur << std::endl;
+		//if ( seed % 10 == 0 ) {
+			//mp_exp_t curExp = getExp(cur);
+			//std::cout << seed << "\t" << cur << "\t" << curExp << std::endl;
+		//}
+		mp_exp_t curExp = getExp(cur);
+		if (curExp > maxExp) {
+			maxExp = curExp;
+		}
+		if (curExp >= 1000) {
+			std::cout << "Term with >1000 digits: " << seed << std::endl;
+			break;
+		}
 	}
+	std::cout << "max exp: " << maxExp << std::endl;
+
+	// compute 2 then recurse
+	
+	//double phi = (1+sqrt(5))/2;
+	
+	//// compute n and n-1, then recurse
+	//int rowCount = 0;
+	//long double prev = 0, cur = 0, temp = 0;
+	//for (int seed = 1; seed < 21; seed++) {
+		//if (rowCount == 0 ) {
+			//cur = floor( (pow(phi,seed) - pow(1-phi,seed)) / sqrt(5) );
+			//int p_seed = seed-1;
+			//prev = floor( (pow(phi,p_seed) - pow(1-phi,p_seed)) / sqrt(5) );
+		//} 
+		//else { // if prev = 1; cur = 2
+			//temp = cur; // temp = 2
+			//cur += prev; // 2+1 = 3
+			//prev = temp; // prev = 2
+			
+		//}
+		//if ( seed % 1 == 0 ) 
+			//std::cout << seed << "\t" << cur << std::endl;
+	//}
 	
 	// fully computed
 	
