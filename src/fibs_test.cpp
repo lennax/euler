6206fe9e51419667e62a06678513c425c7e26861
Lenna Peterson <lennalenna@gmail.com>
2011-08-14 19:18:48 -0400

    p298 has mysterious error for i=12.
    Changes based on _getZeck(12,4) position in constructor?!

--- src/fibs_test.cpp
+++ src/fibs_test.cpp
@@ -10,50 +10,42 @@
 
 mpz_class fibs[8] = {1,2,3,5,8,13,21,34};
 
-int getZeck (mpz_class i, int fibIndex, bool recurse) {
+int _getZeck (mpz_class i, int fibIndex, bool recurse = false) {
 	static int zeck = 1;
 	if ( !recurse ) zeck = 1;
 	mpz_class number = i;
 
-		number -= fibs[fibIndex];
-		//std::cout << fibs[fibIndex] << "\t";
-		zeck++;
-		for ( int x = fibIndex-2; x>-1; x-- ) {
-			if ( number == fibs[x] ) {
-				std::cout << "z: " << zeck << std::endl;
-				return zeck; 
-			}
-			else if ( x > 1 && number > fibs[x] ) {
-				getZeck( number, x, true );
-			}
+	number -= fibs[fibIndex];
+	//std::cout << fibs[fibIndex] << "\t";
+	zeck++;
+	for ( int x = fibIndex-2; x>-1; x-- ) {
+		if ( number == fibs[x] ) {
+			std::cout << "z: " << zeck << std::endl;
+			return zeck; 
 		}
+		else if ( x > 1 && number > fibs[x] ) {
+			_getZeck( number, x, true );
+		}
+	}
 }
 
 int main () {
 	int fibIndex = 0;
 	mpz_class argMax = 35; 
 	for ( mpz_class i = 1; i < argMax; i++ ) {
-		//mpz_class maxFib;
-
-		if ( i == 1 ) {
-			continue;	// 1 is fibIndex 0 and zeck 1
-		}
-		else if ( i == fibs[fibIndex+1] ) {
+		if ( i > 1 && i == fibs[fibIndex+1] ) {
 			fibIndex++; // i is fib so increase index
 		} 
 		//else if ( i > fibs[fibIndex] && i < fibs[fibIndex+1] ) {
-			// i is not fib, so don't increase index
 			// do nothing?! 
 		//}
 		
-		//maxFib = fibs[fibIndex]; 
-
 		std::cout << "i: " << i << "\t";
 		
 		// if i is a fib, zeck is 1, else find zeck
-		int zeck = (i == fibs[fibIndex]) ? 1 : getZeck(i, fibIndex, false);
+		int zeck = (i == fibs[fibIndex]) ? 1 : _getZeck(i, fibIndex);
 		
 		if ( zeck == 1 ) std::cout << "z: 1" << std::endl;
-		//getZeck(i, fibIndex, 0);
+
 	}
 }
--- src/p297.cpp
+++ src/p297.cpp
@@ -9,104 +9,83 @@
 class Zeckendorf
 {
 public:
-	Zeckendorf(mpz_class argMin, mpz_class argMax)
+	Zeckendorf(mpz_class argMax)
 	{
 		// construct vector (99th term is well over 10^19)
-		for ( mpz_class i=argMin; i<100; i++ ) {
+		for ( mpz_class i = 2; i < 100; i++ ) {
 			mpz_class fib = smallFib(i.get_ui());
 			if ( fib <= argMax ) {
 				_fibs.push_back(fib);
-				// std::cout << fib << std::endl;
 			} 
 			else break;	
 		}
-		// std::cout << "Constructed vector <= " << argMax << std::endl;
 		
+		// for (int k=0; k <_fibs.size(); k++) {
+		// 	std::cout << _fibs[k] << std::endl;
+		// }
+
+		mpz_class zeckSum = 0;
+		// find closest fib and pass to _getZeck()
+		int fibIndex = 0;
+		for ( mpz_class i = 1; i < argMax; i++ ) {
+			if ( i > 1 && i == _fibs[fibIndex+1] ) {
+				fibIndex++; // i is fib so increase index
+			} 
+			//else if ( i > _fibs[fibIndex] && i < _fibs[fibIndex+1] ) {
+				// do nothing?! 
+			//}
+
+			// std::cout << "i: " << i << "\t";
+
+			// if i is a fib, zeck is 1, else find zeck
+			int zeck = (i == _fibs[fibIndex]) ? 1 : _getZeck(i, fibIndex);
+
+			// if ( zeck == 1 ) std::cout << "z: 1" << std::endl;
+
+			// std::cout << "i: " << i << "\tfi: " << fibIndex << "\tz: " << zeck << std::endl;
+			zeckSum += zeck;
+		}
+
+		// std::cout << "Sum of Zeckendorf numbers 0<n<" << argMax << ": ";
+		// 	std::cout << zeckSum << std::endl;		
+			
 		// std::cout << _fibs.size() << std::endl;
-		std::cout << _zeckSum(argMin, argMax) << std::endl;;
+		std::cout << _getZeck(12, 4) << std::endl;
 		
 	}
 
 private:
-	mpz_class _zeckSum(mpz_class argMin, mpz_class argMax) {
-		mpz_class zeckSum = 0;
-		for ( mpz_class i = argMin; i < argMax; i++ ) {
-			// std::cout << i << ":\t";
-			zeckSum += _zeck(i);
-		}
-		return zeckSum;
+	std::vector<mpz_class> _fibs;
+
+	int _getZeck (mpz_class i, int fibIndex) {
+		_getZeckRecursive(i, fibIndex, 1);
 	}
 
-	mpz_class _maxFib(mpz_class arg) {
-		for ( mpz_class search = arg; search > 0; search-- ) {
-			if (std::binary_search (_fibs.begin(), _fibs.end(), search)) {
-				// std::cout << "Found " << search << std::endl;
-				return search;
+	int _getZeckRecursive (mpz_class i, int fibIndex, int zeck) {
+		// static int zeck = 1;
+		// if ( !recurse ) zeck = 1;
+		mpz_class number = i;
+
+		number -= _fibs[fibIndex];
+		//std::cout << _fibs[fibIndex] << "\t";
+		zeck++;
+		for ( int x = fibIndex-2; x>-1; x-- ) {
+			if ( number == _fibs[x] ) {
+				// std::cout << "z: " << zeck << std::endl;
+				return zeck; 
+			}
+			else if ( x > 1 && number > _fibs[x] ) {
+				_getZeckRecursive( number, x, zeck );
 			}
 		}
-		std::cout << arg << " not found" << std::endl;
-		exit(1);
 	}
 	
-	mpz_class _zeck(mpz_class arg) {
-		mpz_class zeck = 1;
-		mpz_class number = arg;
-		while (number >= 3) {
-			mpz_class piece = _maxFib(number);
-			// std::cout << piece << " ";
-			if (piece == number) {
-				// std::cout << "\nzeck == " << zeck << std::endl;
-				return zeck;
-			}
-			number -= piece;
-			zeck++;
-			// std::cout << number << "\t" << piece << "\t" <<  zeck << std::endl;
-		}
-		// std::cout << "\nzeck should be " << zeck << std::endl; 
-		return zeck;
-	}
-	
-	std::vector<mpz_class> _fibs;
 };
 
-//uint32_t zeckendorf (double number) {
-	//uint32_t zeck = 1;
-	//while (number > 3) {
-		//mpf_class zeckPiece = fibCeil(number);
-		//if ( zeckPiece == number ) {
-			//std::cout << number << " is fib" << std::endl;
-			//return 1;
-		//}
-		//std::cout << number << "\t" << zeckPiece << "\t" <<  zeck << std::endl;
-		//number -= zeckPiece.get_d();
-		//zeck++;
-	//}
-
-	//return 1;
-//}
-
-
 int main(int argc, char const* argv[]) {
-	// uint32_t arg = 85;
-	if (argc==2) {
-		mpz_class argMax = atoi(argv[1]);
-		Zeckendorf zeckInst(1, argMax);
-	} else {
-		// mpz_class argMax = 10000000000000000000;
-		mpz_class argMax = 10000;
-		
-		Zeckendorf zeckInst (1, argMax);
-	}
-	//zeckendorf(arg);
-	
-	// std::vector<mpz_class> fibs = fibVector(arg);
-	//for ( uint32_t i=0; i<fibs.size(); i++ ) {
-		//std::cout << fibs[i] << std::endl;
-	//}
-	
-	//mpz_class search = 55;
-	
-	// std::cout << maxFib(arg) << std::endl;
-	
+	// use default argument if none is specified
+	mpz_class argMax = (argc == 2 ) ? atoi(argv[1]) : 10000;
+	Zeckendorf zeckInst (argMax);
+
 	return 0;
 }
