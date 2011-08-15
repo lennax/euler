c9a9717fa5abd679afb2841838b2c2af2c1314ea
Lenna Peterson <lennalenna@gmail.com>
2011-08-15 18:41:47 -0400

    Moved MatrixReader to library, p18 works now.
    Just copied PathFinder stuff to p18. Checked 18, 67, 81.

---
+++ src/p18.hpp
@@ -0,0 +1,41 @@
+#ifndef _P18_HPP
+#define _P18_HPP
+
+#include "matrix.hpp"
+
+// abstract class
+class PathFinder {
+	public: 
+		typedef std::vector< std::vector<uint32_t> > matrix_t; 
+		
+		enum pathType_t {
+			PATHTYPE_MIN, PATHTYPE_MAX
+		}; 
+		
+		virtual uint32_t getSum() = 0; 
+		
+		// constructor	
+		PathFinder (matrix_t matrix, pathType_t pathType);
+
+		uint32_t compare(uint32_t a, uint32_t b);
+			
+	private: pathType_t _pathType;
+		
+	protected: matrix_t _matrix; 		
+};
+
+class PathFinderSquare1: public PathFinder {
+	public: 
+		//constructor
+		PathFinderSquare1 (matrix_t matrix, pathType_t pathType);
+		uint32_t getSum();
+};
+
+class PathFinderTriangle1: public PathFinder {
+	public: 
+		//constructor 
+		PathFinderTriangle1 (matrix_t matrix, pathType_t pathType);
+		uint32_t getSum();	
+}; 
+
+#endif // _P18_HPP
---
+++ src/sudoku.cpp
@@ -0,0 +1,17 @@
+// Block 1: for (int y=0; y<3; y++) for (int x=0; x<3; x++)
+// Block 2: for (int y=0; y<3; y++) for (int x=3; x<6; x++)
+// Block 3: for (int y=0; y<3; y++) for (int x=6; x<9; x++)
+// Block 4: for (int y=3; y<6; y++) for (int x=0; x<3; x++)
+// Block 5: for (int y=3; y<6; y++) for (int x=3; x<6; x++)
+// Block 6: for (int y=3; y<6; y++) for (int x=6; x<9; x++)
+// Block 7: for (int y=6; y<9; y++) for (int x=0; x<3; x++)
+// Block 8: for (int y=6; y<9; y++) for (int x=3; x<6; x++)
+// Block 9: for (int y=6; y<9; y++) for (int x=6; x<9; x++)
+//
+// Row i: y=i; for (int x=0; x<9; x++)
+//
+// Col j: x=j; for (int y=0; y<9; y++)
+
+int main () {
+	return 0;
+}
