6c117b13b73cf8e879cfb098fdcc62ae404c0541
Lenna Peterson <lennalenna@gmail.com>
2011-08-29 18:36:38 -0400

    Clarified gprof option.

--- _autobuild.py
+++ _autobuild.py
@@ -19,12 +19,12 @@ def install_build(cmakeargs):
 	subprocess.check_call(["make", makeargs])
 	raise SystemExit
 	
-def in_source_build():
+def dev_build():
 	print "You chose developer build"
 	cmakeargs = " -DIN_SRC_BUILD::bool=TRUE "
 	install_build(cmakeargs)
 
-def grab_dependencies():
+def grab_deps():
 	print "You chose to install dependencies"
 	if 'linux' in sys.platform:
 		subprocess.check_call('sudo apt-get install cmake', shell=True)
@@ -34,6 +34,10 @@ def grab_dependencies():
 		print "Platform not recognized (did not match linux or darwin)"
 	raise SystemExit
 
+# requires PROFILE definition in CMakeLists.txt:
+# set(CMAKE_BUILD_TYPE PROFILE)
+# set(CMAKE_CXX_FLAGS_PROFILE "-g -pg")
+# set(CMAKE_C_FLAGS_PROFILE "-g -pg")
 def profile():
 	print "You chose to compile for gprof"
 	cmakeargs = " -DBUILD_TYPE=PROFILE -DIN_SRC_BUILD::bool=TRUE "
@@ -63,17 +67,16 @@ def clean():
 	# menu()
 	
 def menu():
-	print "1. developer_build: used for development."
-	print "2. install_build: used for building before final installation to the system."
-	print "3. grab_dependencies: installs all the required packages for debian based systems (ubuntu maverick/ debian squeeze,lenny) or darwin with macports."
-	print "4. package_source: creates a source package for distribution."
+	print "1. developer build: used for development."
+	print "2. install build: used for building before final installation to the system."
+	print "3. grab dependencies: installs all the required packages for debian based systems (ubuntu maverick/ debian squeeze,lenny) or darwin with macports."
+	print "4. package source: creates a source package for distribution."
 	print "5. package: creates binary packages for distribution."
 	print "6. remake: calls make again after project has been configured as install or in source build."
 	print "7. clean: removes the build directory."
 	print "8. profile: compiles for gprof."
 	print "9. end."
 	opt = raw_input("Please choose an option: ")
-	# print "Input option: %s" % (opt)
 	return opt
 
 loop_num = 0
@@ -82,17 +85,16 @@ while (1):
 	if len(sys.argv) == 2 and loop_num == 0:
 		opt = sys.argv[1]
 		loop_num += 1
-		# print "Command line option: %s" % (sys.argv[1])
 	else:
 		opt = menu()
 
 	opt = int(opt)
 	if   opt == 1:
-		in_source_build()
+		dev_build()
 	elif opt == 2:
 		install_build(cmakeargs)
 	elif opt == 3: 
-		grab_dependencies()
+		grab_deps()
 	elif opt == 4:
 		package_source()
 	elif opt == 5:
@@ -102,6 +104,7 @@ while (1):
 	elif opt == 7:
 		clean()
 	elif opt == 8:
+		# requires definition in CMakeLists.txt (see def above)
 		profile()
 	elif opt == 9:
 		raise SystemExit
