bac54d1af9d3f34b3e5c53541f45e6001ddbbcc3
Lenna Peterson <arklenna@gmail.com>
2011-09-09 21:21:41 -0400

    Works in C++. Also rewrote in Python.

--- src/bitmask.cpp
+++ src/bitmask.cpp
@@ -1,19 +1,25 @@
 #include <iostream> // for std::cout etc.
 #include <vector>
 #include <inttypes.h> // for uint32_t
+#include <cstdlib> // for atoi()
 
 class Bitmask
 {
 public:
 	// constructor
-	Bitmask(uint32_t n, uint32_t max): 
-	_n(1 << n),
-	_max(max) {
+	Bitmask(uint32_t n): 
+	_n(0), 
+	_size(n) {
 
 	}
 
 	// methods
 	void setBit(uint32_t bit, bool set = true) {
+		if (bit > _size) {
+			std::cerr << "Bit is larger than allowed" << std::endl;
+			std::cerr << "Max size is " << _size << std::endl;
+			return;
+		}
 		if (set) {
 			_n |= 1 << bit;
 		}
@@ -29,7 +35,7 @@ public:
 
 	std::vector<uint32_t> listBits() {
 		std::vector<uint32_t> bitlist;
-		for (uint32_t bit = 0; bit < _max+1; bit++) {
+		for (uint32_t bit = 0; bit < _size+1; bit++) {
 			if (getBit(bit)) {
 				bitlist.push_back(bit);
 			}
@@ -40,29 +46,36 @@ public:
 private:
 	// attributes
 	uint32_t _n;
-	uint32_t _max;
+	uint8_t _size;
 
 };
 
 int main(int argc, char const* argv[]) {
-	uint8_t size = 21;
+	uint8_t default_size = 21;
+	uint8_t size = (argc == 2) ? atoi(argv[1]) : default_size;
 	if (size > 30) {
 		std::cerr << "Error: Size must be less than 31" << std::endl;
 		exit(1);
 	}
 
-	Bitmask bitmaskInstance(size,size);
+	uint8_t combat_mage[] = { 2, 4, 15, 17, 18, 19, 20 };
+
+	Bitmask bitmaskInstance(size);
 
 	// test cases
-	bitmaskInstance.setBit(3);
+	//bitmaskInstance.setBit(3);
 //	std::cout << bitmaskInstance.getBit(2) << std::endl;
 //	std::cout << bitmaskInstance.getBit(3) << std::endl;
 
 
-	bitmaskInstance.setBit(5);
-	bitmaskInstance.setBit(15);
-	bitmaskInstance.setBit(16);
-	std::cout << bitmaskInstance.getBit(15) << std::endl << std::endl;
+	//bitmaskInstance.setBit(5);
+	//bitmaskInstance.setBit(15);
+	//bitmaskInstance.setBit(16);
+	//bitmaskInstance.setBit(size);
+	//std::cout << bitmaskInstance.getBit(15) << std::endl << std::endl;
+	for ( uint8_t i = 0; i < 7; i++ ) {
+		bitmaskInstance.setBit(combat_mage[i]);
+	}
 	std::vector<uint32_t> setlist = bitmaskInstance.listBits();
 	for (uint32_t i = 0; i < setlist.size(); i++) {
 		std::cout << setlist[i] << std::endl;
